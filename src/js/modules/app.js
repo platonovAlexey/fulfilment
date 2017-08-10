7b7708abec98d43754dbc39926485ce23e5efbe2
platonovAlexey <kalyawe4ka@gmail.com>
2017-08-10 18:20:37 +0300

    add adaptive, fix menu

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -30,3 +30,68 @@ $(window).scroll(function() {
 $('#toTop').click(function() {
 	$('body,html').animate({scrollTop:0},800);
 });
+
+var h_hght = 711; 
+var h_mrg = 0;
+
+$(function(){
+ 
+	var elem = $('.top-line2');
+	var top = $(this).scrollTop();
+	 
+	if(top > h_hght){
+		elem.css('top', h_mrg);
+	}
+	 
+	$(window).scroll(function(){
+		top = $(this).scrollTop();
+		 
+		if (top+h_mrg < h_hght) {
+			elem.css('top', (h_hght-top));
+			elem.css('display', 'none');
+		} else {
+			elem.css('top', h_mrg);
+			elem.css('display', 'block');
+		}
+	});
+});
+
+var menu_selector = ".nav2";
+
+function onScroll(){
+	var scroll_top = $(document).scrollTop() + 250;
+	$(menu_selector + " a").each(function(){
+		var hash = $(this).attr("href");
+		var target = $(hash);
+		if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+			$(menu_selector + " a.active").removeClass("active");
+			$(this).addClass("active");
+		} else {
+			$(this).removeClass("active");
+		}
+	});
+}
+
+$(document).ready(function () {
+ 
+	$(document).on("scroll", onScroll);
+ 
+	$("a[href*='#']").click(function(e){
+		e.preventDefault();
+ 
+		$(document).off("scroll");
+		$(menu_selector + " a.active").removeClass("active");
+		$(this).addClass("active");
+		var hash = $(this).attr("href");
+		var target = $(hash);
+ 
+		$("html, body").animate({
+			scrollTop: target.offset().top
+		}, 500, function(){
+			window.location.hash = hash;
+			$(document).on("scroll", onScroll);
+		});
+ 
+	});
+ 
+});
